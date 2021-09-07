806ad433249de76bcb73175e182defd73748bc6a
Daniel <dlandim41@gmail.com>
2021-09-07 20:00:17 -0300

    Add a Livro.class and modify PrimeiroPrograma.java

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,13 +1,18 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
-        Gato gato = new Gato( "Azul","Alfredo",10);
+        Gato gato = new Gato("Azul", "Alfredo", 10);
+
+        Livro livro = new Livro("Ready Player 2", 15);
 
         System.out.println(gato);
+        System.out.println(livro);
+
 //        int a = 5;
 //        int b = 5;
 //        System.out.println("Hello World" + " " + (a + b));
---
+++ src/br/com/dio/model/Livro.java
@@ -0,0 +1,27 @@
+package br.com.dio.model;
+
+public class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+}
