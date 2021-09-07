10c3a50e1f4b90233dcbd098920758fee0c426a8
Daniel <dlandim41@gmail.com>
2021-09-07 19:32:07 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato( "Azul","Alfredo",10);
+
+        System.out.println(gato);
+//        int a = 5;
+//        int b = 5;
+//        System.out.println("Hello World" + " " + (a + b));
+    }
+}
