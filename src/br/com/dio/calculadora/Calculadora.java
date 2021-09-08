44ff7609b6e9038e2e2ce2edc4cb6430cad1141f
Daniel <dlandim41@gmail.com>
2021-09-07 21:06:08 -0300

    Add a file Calculadora.java

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+
+        int somar = somar(a, b);
+        int subtracao = subtracao(a, b);
+        int multiplicacao = multiplicacao(a, b);
+        float divisao = divisao(a, b);
+
+
+        System.out.println("Adição: " + somar);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicacao(int a, int b) {
+        return a * b;
+    }
+
+    public static float divisao(int a, int b) {
+        return a / b;
+    }
+}
